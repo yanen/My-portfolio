a335584687174c06a871177a88f1eaa6c26fc158
yanen <leryanen5@gmail.com>
2020-07-12 01:13:09 +0800

    updated layout added hamburger

---
+++ js/index.js
@@ -0,0 +1,40 @@
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll(".nav__link")
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+
+//smooth scrolling for all browsers using Jquery
+//especially as safari does not support smooth scrolling
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
